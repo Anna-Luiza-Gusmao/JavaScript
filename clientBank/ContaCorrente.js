d9c6054663df579093ea47986a82b00836e078c9
Anna-Luiza-Gusmao <luizasgusmao@hotmail.com>
2022-08-18 16:31:49 -0300

    add_class

--- clientBank/ContaCorrente.js
+++ clientBank/ContaCorrente.js
@@ -1,8 +1,32 @@
+import { Cliente } from "./Cliente.js";
+
 export class ContaCorrente{
+    static numeroDeContas = 0; //faz referência a classe como um todo, e não só a instância de cada classe criada 
     agencia;
-    cliente;
+    _cliente;
+
+    constructor(agencia, cliente){
+        this.agencia = agencia;
+        this._cliente = cliente;
+        ContaCorrente.numeroDeContas += 1; //this se refere a classe que está sendo criada para determinado cliente
+        //ContaCorrente se refere a classe como um todo, no caso para vários clientes 
+    }
+
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor;                                              
+        }
+    }
+
+    get cliente(){
+        return this._cliente;
+    }
 
     _saldo = 0;
+
+    get saldo(){
+        return this._saldo;
+    }
 
     sacar(valor){
         if(this._saldo >= valor){
@@ -17,6 +41,7 @@ export class ContaCorrente{
     }
 
     transferir(valor, conta){
+        conta.cidade = "Montes Claros";
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
     }
--- primeiroProjeto/teste.js
+++
@@ -1,1 +0,0 @@
-#ensinando anna luiza nub 
